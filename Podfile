3bc08d8dc3fcba0c21c924c108597ba0ec567345
daxapps <daxapps777@gmail.com>
2017-02-06 19:00:15 -0600

    setup UI

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/Cocoapods/Specs.git'
+platform :ios, '9.0'
+
+target 'WeatherForecast' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.3'
+
+  # Pods for WeatherForecast
+
+end
---
+++ WeatherForecast/WeatherVC.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherVC.swift
+//  WeatherForecast
+//
+//  Created by Jason Crawford on 2/6/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
